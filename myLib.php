3316c61b477adfebfa713607de10c6a3c732b9a4
rhed2k <rhed2k@gmail.com>
2025-11-12 08:34:54 -0300

    Alterado arquivo myLib para v1.2

--- myLib.php
+++ myLib.php
@@ -1,4 +1,6 @@
 <?php
 //Versão 1 da myLib
-echo "Teste de Library para um exemplo de git"
+echo "Teste de Library para um exemplo de git v1.1\n";
+//Versao 1.2
+echo "Teste de Library para um exemplo de git -v1.2\n";
 ?>
